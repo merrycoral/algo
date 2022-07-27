4530a17d8e2c05111a8dc6c1a8b94aeb779b5562
merrycoral <merrycoral@gmail.com>
2022-07-27 17:37:24 +0900

    그래프 표현

---
+++ Part2/graph_py.py
@@ -0,0 +1,14 @@
+# 파이썬에서 그래프 표현 방식
+# dictionary type을 이용한다.
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C','J']
+graph['J'] = ['I']
